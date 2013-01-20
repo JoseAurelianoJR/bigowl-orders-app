15e06e5092b4a064fc0a4eca719f5f11adae6f32
Lukas Alexandre <lukeskytm@gmail.com>
2013-01-20 01:21:18 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrdersApp::Application.config.secret_token = '882be2ae586673e5499355fb9af1d7ee8df2a06461920e22c19ce25708523df149bf6ddbaabed8c823559c27e5f8efc1578d8b5e3cdfc9fcef8067a9dc1d57a0'
